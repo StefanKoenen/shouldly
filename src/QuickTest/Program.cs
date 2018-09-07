b991d85a42540492a3ed5bb2a817cc3d96805ac2
Unknown <stefan.koenen1@gmail.com>
2018-09-07 19:38:21 +0200

    Changed target framework, and added console app for testing.

---
+++ src/QuickTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (9).ShouldNotBeAssignableTo<UInt32>(); // Pass
+            (9).ShouldNotBeAssignableTo<byte>(); // Pass
+            (0xFF).ShouldNotBeAssignableTo<byte>(); // Pass
+        }
+    }
+}
